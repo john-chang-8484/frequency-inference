e8fb00043b4d353f5b75cf0ba0ed4bcf2bf98119
Phillip B <phillipb163@gmail.com>
2019-07-09 10:46:04 +0100

    add profiling

---
+++ do_profile.py
@@ -0,0 +1,4 @@
+import sim
+import cProfile
+cProfile.run('sim.main()', 'sim_stats.stats')
+
---
+++ show_profile.py
@@ -0,0 +1,8 @@
+import pstats
+from pstats import SortKey
+
+p = pstats.Stats('sim_stats.stats')
+
+# top ten time consuming functions
+p.strip_dirs().sort_stats(SortKey.TIME).print_stats(10)
+
