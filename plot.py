2c27211f23e962ce71a505f0b466e8a389c69fff
Phillip Bement <phillipb163@gmail.com>
2019-07-25 12:00:11 +0100

    smaller points

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ def plot_measure_time(b):
     for i, run_hist, loss, nm in zip(range(n_estimators), b.run_hists, b.avg_losses, b.estimator_names):
         plt.errorbar(b.tlist, loss, yerr=np.sqrt(b.avg_loss_vars[i])/np.sqrt(b.runs), capsize=2, label=nm, color=colours[i])
         percentiles = np.percentile(run_hist, which_percentiles, axis=1)
-        plt.plot(b.tlist.reshape((1,) + b.tlist.shape).repeat(which_percentiles.size, 0), percentiles, marker='o', linestyle='None', color=colours[i])
+        plt.plot(b.tlist.reshape((1,) + b.tlist.shape).repeat(which_percentiles.size, 0), percentiles, marker='o', linestyle='None', color=colours[i], markersize=3)
     #plt.ylim(bottom=0.0)
     plt.plot(b.tlist, b.var_omega * (np.sqrt(1 + 4/(b.tlist**2*b.var_omega)) - 1) / 2, label='Cramer Rao bound') # Bayesian Cramer Rao Bound
     plt.plot(b.tlist, b.tlist*0 + ((b.omega_max - b.omega_min) / b.NUM_PARTICLES)**2 / 12, label='grid bound')
@@ -43,11 +43,11 @@ def plot_measurement_performance(b):
     n_estimators = b.run_hists.shape[0]
     colourmap = plt.get_cmap('jet')
     colours = [colourmap(k) for k in np.linspace(0., 1., n_estimators)]
-    which_percentiles = np.array([])#[25, 50, 75, 90, 99])
+    which_percentiles = np.array([25, 50, 75])
     for i, run_hist, loss, nm in zip(range(n_estimators), b.run_hists, b.avg_losses, b.estimator_names):
         plt.errorbar(b.N_list, loss, yerr=np.sqrt(b.avg_loss_vars[i])/np.sqrt(b.runs), capsize=2, label=nm, color=colours[i])
         percentiles = np.percentile(run_hist, which_percentiles, axis=1)
-        plt.plot(b.N_list.reshape((1,) + b.N_list.shape).repeat(which_percentiles.size, 0), percentiles, marker='o', linestyle='None', color=colours[i])
+        plt.plot(b.N_list.reshape((1,) + b.N_list.shape).repeat(which_percentiles.size, 0), percentiles, marker='o', linestyle='None', color=colours[i], markersize=3)
     plt.plot(b.N_list, b.N_list*0 + ((b.omega_max - b.omega_min) / b.NUM_PARTICLES)**2 / 12, label='grid bound')
     plt.plot(b.N_list, b.N_list*0 + 3*b.var_omega, label='an estimated bound')
     plt.yscale('log')
